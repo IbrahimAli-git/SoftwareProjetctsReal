c4c1fbc9d39092608d8536d16dd4222a3ad9b22a
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:11:40 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import model.MainModel;
+import presenter.MainPresenter;
+import view.MainView;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        MainModel mainModel = new MainModel();
+        MainPresenter mainPresenter = new MainPresenter(mainModel);
+        MainView mainView = new MainView(mainPresenter);
 
+        mainView.run();
+        
+        try {
+            mainModel.close();
+        } catch (IOException e) {
+        }
     }
 }
+
