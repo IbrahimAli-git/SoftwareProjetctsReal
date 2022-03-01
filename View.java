e2ee83c0904aa8639d6628b186a8b2d027531bb5
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:12:13 +0000

    Update View.java

--- View.java
+++ View.java
@@ -7,13 +7,14 @@ import java.sql.SQLException;
 public interface View {
 
 
-    void create(Connection con, int overallPerformance) throws Exception;
+    void create(int overallPerformance) throws Exception;
 
     int calculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws Exception;
 
-    void retrieve(Connection connection, int id) throws SQLException;
+    void retrieve(int id) throws SQLException;
 
-    void update(Connection connection, BufferedReader br) throws Exception;
+    void update(BufferedReader br) throws Exception;
 
-    void delete(Connection connection, BufferedReader br) throws Exception;
+    void delete(BufferedReader br) throws Exception;
 }
+
