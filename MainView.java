56ee4d7b053ee5c33d84381d4f27d0a1c21db605
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:11:18 +0000

    Update MainView.java

--- MainView.java
+++ MainView.java
@@ -9,14 +9,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MainView implements View {
-
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/softwareprojectdatabase";
-    private static final String USER_NAME = "root";
-    private static final String PASSWORD = "WR*=ACHoXo0$c$j+wrlt";
     private MainPresenter mainPresenter;
 
-    public static void main(String[] args) {
 
+    public MainView(MainPresenter mainPresenter) {
+        this.mainPresenter = mainPresenter;
+    }
+
+    public void create(int overallPerformance) throws Exception {
+        mainPresenter.onCreate(overallPerformance);
+    }
+
+    public int calculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws Exception {
+        return mainPresenter.onCalculateOverallPerformance(bufferedReader, attendance, courseWorkMarks, examMarks);
+    }
+
+    public void retrieve(int id) throws SQLException {
+        mainPresenter.onRetrieve(id);
+    }
+
+    public void update(BufferedReader br) throws Exception {
+        mainPresenter.onUpdate(br);
+    }
+
+    public void delete(BufferedReader br) throws Exception {
+        mainPresenter.onDelete(br);
+    }
+
+    public void run() {
         System.out.println("Welcome to the program");
         System.out.println("Please choose an option below:");
         System.out.println("Enter 1 for evaluating overall performance:");
@@ -25,12 +45,8 @@ public class MainView implements View {
         System.out.println("Enter 4 for deleting a student's record");
         System.out.println("---------------------------------------------------------");
 
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            Connection connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);)
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));)
         {
-            MainView mainView = new MainView();
-
-
             System.out.println("Please enter option");
             int option = Integer.parseInt(bufferedReader.readLine());
             System.out.println();
@@ -39,43 +55,24 @@ public class MainView implements View {
                 int attendance = 0;
                 int courseWorkMarks = 0;
                 int examMarks = 0;
-                int overallPerformance = mainView.calculateOverallPerformance(bufferedReader, attendance, courseWorkMarks, examMarks);
-                mainView.create(connection, overallPerformance);
+                int overallPerformance = calculateOverallPerformance(bufferedReader, attendance, courseWorkMarks, examMarks);
+                create(overallPerformance);
 
             }
             if (option == 2) {
                 System.out.println("Please enter student id:");
                 int idOfStudent = Integer.parseInt(bufferedReader.readLine());
-                mainView.retrieve(connection, idOfStudent);
+                retrieve(idOfStudent);
             }
             if (option == 3) {
-                mainView.update(connection, bufferedReader);
+                update(bufferedReader);
             }
             if (option == 4) {
-                mainView.delete(connection, bufferedReader);
+                delete(bufferedReader);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error performing operations \n Please try again");
         }
     }
 
-    public void create(Connection con, int overallPerformance) throws Exception {
-        mainPresenter.onCreate(con, overallPerformance);
-    }
-
-    public int calculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws Exception {
-        return mainPresenter.onCalculateOverallPerformance(bufferedReader, attendance, courseWorkMarks, examMarks);
-    }
-
-    public void retrieve(Connection connection, int id) throws SQLException {
-        mainPresenter.onRetrieve(connection, id);
-    }
-
-    public void update(Connection connection, BufferedReader br) throws Exception {
-        mainPresenter.onUpdate(connection, br);
-    }
-
-    public void delete(Connection connection, BufferedReader br) throws Exception {
-        mainPresenter.onDelete(connection, br);
-    }
 }
