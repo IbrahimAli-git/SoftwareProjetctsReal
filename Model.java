c51ba34c9b41c08fc376f16cc39c74647fc19b1e
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:12:58 +0000

    Update Model.java

--- Model.java
+++ Model.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 
 public interface Model {
 
-    void create(Connection con, int overallPerformance) throws SQLException;
+    void create(int overallPerformance) throws SQLException;
     int calculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws IOException;
-    void update(Connection connection, BufferedReader br) throws SQLException, IOException;
-    void delete(Connection connection, BufferedReader br) throws SQLException, IOException;
-    void retrieve(Connection connection, int id) throws SQLException;
+    void update(BufferedReader br) throws SQLException, IOException;
+    void delete(BufferedReader br) throws SQLException, IOException;
+    void retrieve(int id) throws SQLException;
 }
+
