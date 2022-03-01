ffcab60842b4bd3162697e4e581e4468a7cf4b7b
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:09:48 +0000

    Update MainPresenter.java

--- MainPresenter.java
+++ MainPresenter.java
@@ -9,23 +9,27 @@ import java.sql.SQLException;
 public class MainPresenter {
     private MainModel mainModel;
 
-    public void onCreate(Connection con, int overallPerformance) throws Exception {
-        mainModel.create(con, overallPerformance);
+    public MainPresenter(MainModel mainModel) {
+        this.mainModel = mainModel;
+    }
+
+    public void onCreate(int overallPerformance) throws Exception {
+        mainModel.create(overallPerformance);
     }
 
     public int onCalculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws Exception {
         return mainModel.calculateOverallPerformance(bufferedReader, attendance, courseWorkMarks, examMarks);
     }
 
-    public void onRetrieve(Connection connection, int id) throws SQLException {
-        mainModel.retrieve(connection, id);
+    public void onRetrieve(int id) throws SQLException {
+        mainModel.retrieve(id);
     }
 
-    public void onUpdate(Connection connection, BufferedReader br) throws Exception {
-        mainModel.update(connection, br);
+    public void onUpdate(BufferedReader br) throws Exception {
+        mainModel.update(br);
     }
 
-    public void onDelete(Connection connection, BufferedReader br) throws Exception {
-        mainModel.delete(connection, br);
+    public void onDelete(BufferedReader br) throws Exception {
+        mainModel.delete(br);
     }
 }
