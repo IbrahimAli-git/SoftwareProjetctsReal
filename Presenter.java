3b1897c47b446b4e15ebd1b7bdca608b265f521b
Ibrahim Mohamud Ali <ibrahim2ali169@gmail.com>
2022-03-01 14:12:38 +0000

    Update Presenter.java

--- Presenter.java
+++ Presenter.java
@@ -6,15 +6,13 @@ import java.sql.SQLException;
 
 public interface Presenter {
 
-
-
-    void onCreate(Connection con, int overallPerformance) throws Exception;
+    void onCreate(int overallPerformance) throws Exception;
 
     int onCalculateOverallPerformance(BufferedReader bufferedReader, int attendance, int courseWorkMarks, int examMarks) throws Exception;
 
-    void onRetrieve(Connection connection, int id) throws SQLException;
+    void onRetrieve(int id) throws SQLException;
 
-    void onUpdate(Connection connection, BufferedReader br) throws Exception;
+    void onUpdate(BufferedReader br) throws Exception;
 
-    void onDelete(Connection connection, BufferedReader br) throws Exception;
+    void onDelete(BufferedReader br) throws Exception;
 }
